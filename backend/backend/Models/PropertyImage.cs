35efd482d92b2eb6e7a87ca0aabf5093343bd027
Nils Christian Danielsen <ncdanielsen@live.co.uk>
2020-03-09 16:15:51 +0100

    Filled out data annotations for several models #18

--- backend/backend/Models/PropertyImage.cs
+++ backend/backend/Models/PropertyImage.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -9,9 +10,12 @@ namespace backend.Models
     public class PropertyImage
     {
         public int Id { get; set; }
+        [Required]
+        [Column(TypeName = "nvarchar(200)")]
         public string Url { get; set; }
-
+        [Required]
         public int PropertyId { get; set; }
+        [Required]
         public Property Property { get; set; }
     }
 }
