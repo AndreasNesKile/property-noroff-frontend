f5147ed223bffae3fb6b1f96d6255623687a3da1
Lars Tollefsen <larsthomastt@gmail.com>
2020-03-11 13:01:46 +0100

    Test data hardcoded instead of getting from a state array. Fix later

--- frontend/src/components/PropertyCatalogue/PropertyCatalogue.js
+++ frontend/src/components/PropertyCatalogue/PropertyCatalogue.js
@@ -4,11 +4,8 @@ import Property from "../Property/Property"
 
 
 class PropertyCatalogue extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      properties: [{name: 'Zero to one',isbn: '9780804139298',author: 'Peter Thiel',cover: 'https://images.gr-assets.com/books/1414347376l/18050143.jpg',status: false},{name: "The Manager's Path",isbn: '9781491973899',author: 'Camille Fournier',cover: 'https://images.gr-assets.com/books/1484107737l/33369254.jpg',status: false},{name: 'Calculus, Better Explained',isbn: '9781470070700',author: 'Kalid Azad',cover: 'https://images.gr-assets.com/books/1448590460l/27993945.jpg',status: false}]
-    };  
+  state = {
+      properties: [{name: 'Zero to one',isbn: '9780804139298',author: 'Peter Thiel',cover: 'https://images.gr-assets.com/books/1414347376l/18050143.jpg',status: false},{name: "The Manager's Path",isbn: '9781491973899',author: 'Camille Fournier',cover: 'https://images.gr-assets.com/books/1484107737l/33369254.jpg',status: false},{name: 'Calculus, Better Explained',isbn: '9781470070700',author: 'Kalid Azad',cover: 'https://images.gr-assets.com/books/1448590460l/27993945.jpg',status: false}] 
   }
 
   render() {
