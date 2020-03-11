8e8b4b64e0332da3b6c4cc4b365b0d6b6eac471e
Lars Tollefsen <larsthomastt@gmail.com>
2020-03-11 14:31:29 +0100

    Bug fixed and all data collection working

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import React from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Property from "./components/Property/Property"
+import PropertyCatalogue from "./components/PropertyCatalogue/PropertyCatalogue"
 import landingpage from './components/landing-page/landing-page';
 import { BrowserRouter as Router, Switch, Route} from "react-router-dom"
 
@@ -13,9 +14,9 @@ function App() {
         <Switch>
           <Route exact path="/" component={landingpage} />
           <Route path="/Property/:id" component={Property} />
-          {/* <Route path="/Properties" component={} />
+          <Route path="/Properties" component={PropertyCatalogue} />
           
-          <Route path="/Profile" component={} /> */}
+          {/*<Route path="/Profile" component={} /> */}*/}
         </Switch>
       </div>
     </Router>
