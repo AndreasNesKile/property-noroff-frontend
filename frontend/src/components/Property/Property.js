c9a0306a7bcdb65c478f48abb13f2b1594f982e4
Lars Tollefsen <larsthomastt@gmail.com>
2020-03-18 10:46:07 +0100

    Working RecentlyViewed based on sessionStorage

--- frontend/src/components/Property/Property.js
+++ frontend/src/components/Property/Property.js
@@ -5,18 +5,34 @@ import { Link } from "react-router-dom"
 import Card from 'react-bootstrap/Card'
 import Button from 'react-bootstrap/Button'
 
+
+
 function Property(props) {
-    console.log(props)
+
+    function handleClick() {
+        if (sessionStorage.getItem("Rview1") === null) {
+            sessionStorage.setItem("Rview1",props.index)
+        } else if(sessionStorage.getItem("Rview2") === null) {
+            sessionStorage.setItem("Rview2",sessionStorage.getItem("Rview1"))
+            sessionStorage.setItem("Rview1",props.index)
+        } else {
+            sessionStorage.setItem("Rview3",sessionStorage.getItem("Rview2"))
+            sessionStorage.setItem("Rview2",sessionStorage.getItem("Rview1"))
+            sessionStorage.setItem("Rview1",props.index)
+        }
+    }
+
     return (
         <Card className={styles.CardContainer} >
             <Card.Img variant="top" src={props.data.image} />
             <Card.Body>
                 <Card.Title>{props.data.address.city}</Card.Title>
                 <Card.Text>{props.data.address.street}</Card.Text>
-                <Button variant="primary">More information</Button>
+                <Button onClick={handleClick} variant="primary">More information</Button>
             </Card.Body>
         </Card>
     );
 };
+
 
 export default Property;
--- frontend/src/components/PropertyCatalogue/PropertyCatalogue.js
+++ frontend/src/components/PropertyCatalogue/PropertyCatalogue.js
@@ -11,16 +11,32 @@ import CardDeck from 'react-bootstrap/CardDeck'
 class PropertyCatalogue extends React.Component {
   state = {
       properties: [{address: {city: "Oslo", street: "Stoopid street 14"},image: 'https://dms-cf-02.dimu.org/image/02349Seijwma?dimension=250x250'},{address: {city: "Oslo", street: "Memelane 69"},image: 'https://ringutleier.fra1.digitaloceanspaces.com/wp-content/uploads/2020/01/1-5-250x250.jpg'},{address: {city: "Sarpsborg", street: "Osloveien 24"},image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/1/17/Bassengveien%2C_Ringerike_-_001.jpg/250px-Bassengveien%2C_Ringerike_-_001.jpg'},{address: {city: "Stavanger", street: "Stavangergata 10"},image: 'https://solidmesterbygg.no/wp-content/uploads/2019/11/69442978_1105638476300316_4997689091689545728_n-250x250.jpg'},{address: {city: "Oslo", street: "Askeladdengata 4"},image: 'https://fra1.digitaloceanspaces.com/ringutleier/wp-content/uploads/2020/02/19232117/D431C005-C4FF-472C-9C2E-0E59B22A4274-250x250.jpeg'},{address: {city: "Oslo", street: "Stoopid street 14"},image: 'https://dms-cf-02.dimu.org/image/02349Seijwma?dimension=250x250'},{address: {city: "Oslo", street: "Memelane 69"},image: 'https://ringutleier.fra1.digitaloceanspaces.com/wp-content/uploads/2020/01/1-5-250x250.jpg'},{address: {city: "Sarpsborg", street: "Osloveien 24"},image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/1/17/Bassengveien%2C_Ringerike_-_001.jpg/250px-Bassengveien%2C_Ringerike_-_001.jpg'},{address: {city: "Stavanger", street: "Stavangergata 10"},image: 'https://solidmesterbygg.no/wp-content/uploads/2019/11/69442978_1105638476300316_4997689091689545728_n-250x250.jpg'},{address: {city: "Oslo", street: "Askeladdengata 4"},image: 'https://fra1.digitaloceanspaces.com/ringutleier/wp-content/uploads/2020/02/19232117/D431C005-C4FF-472C-9C2E-0E59B22A4274-250x250.jpeg'},{address: {city: "Oslo", street: "Stoopid street 14"},image: 'https://dms-cf-02.dimu.org/image/02349Seijwma?dimension=250x250'},{address: {city: "Oslo", street: "Memelane 69"},image: 'https://ringutleier.fra1.digitaloceanspaces.com/wp-content/uploads/2020/01/1-5-250x250.jpg'},{address: {city: "Sarpsborg", street: "Osloveien 24"},image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/1/17/Bassengveien%2C_Ringerike_-_001.jpg/250px-Bassengveien%2C_Ringerike_-_001.jpg'},{address: {city: "Stavanger", street: "Stavangergata 10"},image: 'https://solidmesterbygg.no/wp-content/uploads/2019/11/69442978_1105638476300316_4997689091689545728_n-250x250.jpg'},{address: {city: "Oslo", street: "Askeladdengata 4"},image: 'https://fra1.digitaloceanspaces.com/ringutleier/wp-content/uploads/2020/02/19232117/D431C005-C4FF-472C-9C2E-0E59B22A4274-250x250.jpeg'},{address: {city: "Oslo", street: "Stoopid street 14"},image: 'https://dms-cf-02.dimu.org/image/02349Seijwma?dimension=250x250'},{address: {city: "Oslo", street: "Memelane 69"},image: 'https://ringutleier.fra1.digitaloceanspaces.com/wp-content/uploads/2020/01/1-5-250x250.jpg'},{address: {city: "Sarpsborg", street: "Osloveien 24"},image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/1/17/Bassengveien%2C_Ringerike_-_001.jpg/250px-Bassengveien%2C_Ringerike_-_001.jpg'},{address: {city: "Stavanger", street: "Stavangergata 10"},image: 'https://solidmesterbygg.no/wp-content/uploads/2019/11/69442978_1105638476300316_4997689091689545728_n-250x250.jpg'},{address: {city: "Oslo", street: "Askeladdengata 4"},image: 'https://fra1.digitaloceanspaces.com/ringutleier/wp-content/uploads/2020/02/19232117/D431C005-C4FF-472C-9C2E-0E59B22A4274-250x250.jpeg'},] ,
-      recentproperties: [{address: {city: "Oslo", street: "Stoopid street 14"},image: 'https://dms-cf-02.dimu.org/image/02349Seijwma?dimension=250x250'},{address: {city: "Oslo", street: "Memelane 69"},image: 'https://ringutleier.fra1.digitaloceanspaces.com/wp-content/uploads/2020/01/1-5-250x250.jpg'},{address: {city: "Sarpsborg", street: "Osloveien 24"},image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/1/17/Bassengveien%2C_Ringerike_-_001.jpg/250px-Bassengveien%2C_Ringerike_-_001.jpg'}] 
+      recentproperties: []
+  }
+
+  SetRecentlyViewed() {
+    if (sessionStorage.getItem("Rview3") != null) {
+      this.state.recentproperties.push(this.state.properties[sessionStorage.getItem("Rview3")]);
+      this.state.recentproperties.push(this.state.properties[sessionStorage.getItem("Rview2")]);
+      this.state.recentproperties.push(this.state.properties[sessionStorage.getItem("Rview1")]);
+    } else if(sessionStorage.getItem("Rview2") != null) {
+      this.state.recentproperties.push(this.state.properties[sessionStorage.getItem("Rview2")]);
+      this.state.recentproperties.push(this.state.properties[sessionStorage.getItem("Rview1")]);
+    } else if(sessionStorage.getItem("Rview1") != null) {
+      this.state.recentproperties.push(this.state.properties[sessionStorage.getItem("Rview1")]);
+    } else {
+      console.log("Nothing to report chiefieboy")
+    }
   }
 
   render() {
     const propertycards = this.state.properties.map((data, index) => {
       return (
-        <Property key={index} data={data} />
+        <Property key={index} index={index} data={data} />
       );
     });
 
+    this.SetRecentlyViewed()
     const recentlyviewed = this.state.recentproperties.map((data, index) => {
       return (
         <RecentlyViewed key={index} data={data} />
@@ -34,7 +50,7 @@ class PropertyCatalogue extends React.Component {
         <div className={styles.RecentlyViewedContainer}>
           <div className={styles.RecentlyViewed}>
             <h4>Recently viewed:</h4>
-            <div className="Properties">{recentlyviewed}</div>
+            <div className="Properties">{recentlyviewed.reverse()}</div>
           </div>
         </div>
 
