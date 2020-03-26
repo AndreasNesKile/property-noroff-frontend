9a38696ee0e02bf17535461d933de8136bd9ea94
MrHaika <morganra92@hotmail.com>
2020-03-26 12:39:51 +0100

    fixed account management put request#93

--- frontend/src/components/UserManagement/UserManagement.js
+++ frontend/src/components/UserManagement/UserManagement.js
@@ -1,30 +1,77 @@
-import React from "react";
-import styles from "./UserManagement.module.css";
-import Form from 'react-bootstrap/Form'
-import Button from 'react-bootstrap/Button'
-
+import React from 'react';
+import styles from './UserManagement.module.css';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import axios from 'axios';
+let Api_Url = `http://localhost:5000/api/account/1`;
 class UserManagement extends React.Component {
-  state = {
-  }
+	state = {
+		user: {},
+		config: {
+			headers: {
+				Authorization: `Bearer ${sessionStorage.getItem('token')}`
+			}
+		}
+	};
 
-  render() {
-    return (
-      <div className={styles.FormContainer}>
-        <h1>Account details</h1>
-        <Form >
-          <Form.Group>
-            <Form.Label>First name</Form.Label>
-            <Form.Control type="text" placeholder="First name" />
-            <Form.Label>Last name</Form.Label>
-            <Form.Control type="text" placeholder="Last name" />
-            <Form.Label>Phone number</Form.Label>
-            <Form.Control type="text" placeholder="Phone number" />
-            {/* TODO: Add date of birth changeythingy */}
-          </Form.Group>
-        </Form>
-      </div>
-    );
-  }
+	async componentDidMount() {
+		try {
+			await axios.get(Api_Url, this.state.config ? this.state.config : '').then((res) => {
+				this.setState({ user: res.data });
+				console.log(this.state.user);
+			});
+		} catch (e) {
+			console.log(e);
+		}
+	}
+
+	saveChanges() {
+		axios.put(Api_Url, this.state.user, this.state.config).then((res) => {
+			console.log(res);
+		});
+	}
+	UpdateName(e) {
+		let newSelected = Object.assign({}, this.state.user);
+		newSelected.name = e.target.value;
+		this.setState({ user: newSelected });
+	}
+	UpdateSurname(e) {
+		let newSelected = Object.assign({}, this.state.user);
+		newSelected.surname = e.target.value;
+		this.setState({ user: newSelected });
+	}
+	render() {
+		return (
+			<div className={styles.FormContainer}>
+				<h1>Account details</h1>
+				<Form onSubmit={() => this.saveChanges()}>
+					<Form.Group>
+						<Form.Label>First name</Form.Label>
+						<Form.Control
+							type="text"
+							placeholder="First name"
+							value={this.state.user.name ? this.state.user.name : ''}
+							onChange={(e) => this.UpdateName(e)}
+						/>
+						<Form.Label>Last name</Form.Label>
+						<Form.Control
+							type="text"
+							placeholder="Last name"
+							value={this.state.user.surname ? this.state.user.surname : ''}
+							onChange={(e) => this.UpdateSurname(e)}
+						/>
+						<Form.Group>
+							<Button variant="success" className="mt-1" type="submit">
+								Save Changes
+							</Button>
+						</Form.Group>
+
+						{/* TODO: Add date of birth changeythingy */}
+					</Form.Group>{' '}
+				</Form>
+			</div>
+		);
+	}
 }
 
 export default UserManagement;
